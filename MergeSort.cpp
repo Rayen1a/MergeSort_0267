90b6f793604cf3b66d2b1f2c0be85b4bcab6447a
Rayen1a <raihan01426@gmail.com>
2025-04-24 11:11:24 +0700

    Memasukkan arr

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -55,3 +55,16 @@ void mergeSort(int low, int high)
     int k = low;      // step 4.c
 
     while (i <= mid && j <= high) // step 4.d
+    {
+        if (arr[i] <= arr[j]) // step 4.d.i
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++; // step 4.d.ii
+    }
